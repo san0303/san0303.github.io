1ceafc28378a3a7ca01ec42285814d807230e2da
sansan720303 <sansan4758630@gmail.com>
2023-01-05 17:29:19 +0800

    33

---
+++ smart/js/data.js
@@ -0,0 +1,214 @@
+function getTabContents() {
+  return [
+    {
+      title: '基金單筆申購流程教學',
+      subtitle: '簡單幾步驟 快速完成基金單筆申購',
+      buttonText: '立即申購 &gt;',
+      steps: [
+        {
+          id: 1,
+          content: '行動銀行首頁點基金單筆申購',
+          image: 'img/1-1.png'
+        },
+        {
+          id: 2,
+          content: '選擇單筆申購',
+          image: 'img/1-2.png'
+        },
+        {
+          id: 3,
+          content: '選擇欲申購基金標的',
+          image: 'img/1-3.png'
+        },
+        {
+          id: 4,
+          content: '輸入基金申購設定',
+          image: 'img/1-4.png'
+        },
+        {
+          id: 5,
+          content: '閱讀並確認基金申購相關文件',
+          image: 'img/1-5.png'
+        },
+        {
+          id: 6,
+          content: '確認交易資訊並輸入交易密碼',
+          image: 'img/1-6.png'
+        },
+        {
+          id: 7,
+          content: '交易成功',
+          image: 'img/1-7.png'
+        }
+      ]
+    },
+    {
+      title: '百元投資申購流程教學',
+      subtitle: '日期金額自由選 百元投資輕鬆開始',
+      buttonText: '立即申購 &gt;',
+      steps: [
+        {
+          id: 1,
+          content: '首頁點熱門基金',
+          image: 'img/2-1.png'
+        },
+        {
+          id: 2,
+          content: '預設已為百元投資頁面',
+          image: 'img/2-2.png'
+        },
+        {
+          id: 3,
+          content: '選擇欲申購基金之資料且設定每月扣款日期',
+          image: 'img/2-3.png'
+        },
+        {
+          id: 4,
+          content: '閱讀並確認基金申購相關文件',
+          image: 'img/2-4.png'
+        },
+        {
+          id: 5,
+          content: '確認交易資訊並輸入交易密碼',
+          image: 'img/2-5.png'
+        },
+        {
+          id: 6,
+          content: '交易成功',
+          image: 'img/2-6.png'
+        }
+      ]
+    },
+    {
+      title: '基金總覽查詢',
+      subtitle: '基金資產種類多 元大行動銀行一手掌握',
+      buttonText: '了解更多 &gt;',
+      steps: [
+        {
+          id: 1,
+          content: '行動銀行首頁下方點選基金',
+          image: 'img/3-1.png'
+        },
+        {
+          id: 2,
+          content: '進入基金總覽頁可察看各庫存標的之績效及交易明細',
+          image: 'img/3-2.png'
+        },
+        {
+          id: 3,
+          content: '1.切換申購清單，可以瀏覽各種投資方式及其庫存標的現況<br />2.點選交易編號可以查看訂單資訊',
+          image: 'img/3-3.png'
+        },
+        {
+          id: 4,
+          content: '訂單資訊中，可切換「持有明細」與「申請資訊」，可以查看更詳細資訊 (ex.手續費、配息…等)',
+          image: 'img/3-4.png'
+        }
+      ]
+    },
+    {
+      title: '基金到價通知設定',
+      subtitle: '基金價格即時通知 趨勢追蹤好幫手',
+      buttonText: '了解更多 &gt;',
+      steps: [
+        {
+          id: 1,
+          content: '行動銀行首頁點選設定',
+          image: 'img/4-1.png'
+        },
+        {
+          id: 2,
+          content: '點選基金到價通知設定',
+          image: 'img/4-2.png'
+        },
+        {
+          id: 3,
+          content: '點選新增',
+          image: 'img/4-3.png'
+        },
+        {
+          id: 4,
+          content: '設定欲通知標的/淨值上下限/通知起訖日',
+          image: 'img/4-4.png'
+        },
+        {
+          id: 5,
+          content: '標的通知設定成功',
+          image: 'img/4-5.png'
+        },
+        {
+          id: 6,
+          content: '點選行動銀行設定',
+          image: 'img/4-6.png'
+        },
+        {
+          id: 7,
+          content: '點選訊息通知設定',
+          image: 'img/4-7.png'
+        },
+        {
+          id: 8,
+          content: '開啟到價通知',
+          image: 'img/4-8.png'
+        },
+        {
+          id: 9,
+          content: '將基金商品到價通知選項打勾',
+          image: 'img/4-9.png'
+        }
+      ]
+    },
+    {
+      title: '基金損益通知設定',
+      subtitle: '損益情況到點提醒 輕鬆維持投資策略',
+      buttonText: '了解更多 &gt;',
+      steps: [
+        {
+          id: 1,
+          content: '行動銀行首頁點選設定',
+          image: 'img/5-1.png'
+        },
+        {
+          id: 2,
+          content: '點選基金損益通知設定',
+          image: 'img/5-2.png'
+        },
+        {
+          id: 3,
+          content: '點選新增',
+          image: 'img/5-3.png'
+        },
+        {
+          id: 4,
+          content: '設定投資類型/基金標的/報酬率上下限/通知起訖日',
+          image: 'img/5-4.png'
+        },
+        {
+          id: 5,
+          content: '標的通知設定成功',
+          image: 'img/5-5.png'
+        },
+        {
+          id: 6,
+          content: '點選行動銀行設定',
+          image: 'img/5-6.png'
+        },
+        {
+          id: 7,
+          content: '點選訊息通知設定',
+          image: 'img/5-7.png'
+        },
+        {
+          id: 8,
+          content: '開啟到價通知',
+          image: 'img/5-8.png'
+        },
+        {
+          id: 9,
+          content: '將基金單筆損益通知選項打勾',
+          image: 'img/5-9.png'
+        }
+      ]
+    }
+  ];
+}
