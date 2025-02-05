636a71c1f39925356400b41be77a58058c2baac8
san0303 <70562832+san0303@users.noreply.github.com>
2025-02-05 13:12:44 +0800

    33

---
+++ 0050FF/assets/js/uno_main.js
@@ -0,0 +1,5 @@
+window.__unocss = {
+  rules: [
+    ['custom-btn', { 'background-color': '#000', color: '#fff', padding: '10px 20px', 'border-radius': '5px', 'text-transform': 'uppercase' }],
+  ],
+};
